c6b753cd6597a6c044982ab114ccb182ea5a59ca
sapinskise <19euro@gmail.com>
2022-09-29 14:06:36 +0100

    go to string

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,9 +8,23 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        static void Main()
         {
-            //
+            Program myProg = new Program();
+
+            //int[] ii = {1,2,3,4,5};
+            //Console.WriteLine(ii.Count());
+            //foreach (int i in ii)
+            //    Console.WriteLine(i);
+
+
+            string s1 = "   abc dvsdf dfbd e ";
+
+            s1.IndexOf()
+            
+            Console.WriteLine(s1.Replace(" ", "  ").Length - s1.Length);
+            
         }
     }
 }
