c34dd2131839b90af36246867c476159f012d91a
sapinskise <19euro@gmail.com>
2022-09-29 15:07:50 +0100

    initial

---
+++ Methods6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+        }
+    }
+}
