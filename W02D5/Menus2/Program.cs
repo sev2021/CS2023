db73828759f2b62a42f42bf860d8d3817c9baa3a
sapinskise <19euro@gmail.com>
2022-09-30 10:44:15 +0100

    Random class used to

--- W02D5/Menus2/Program.cs
+++ W02D5/Menus2/Program.cs
@@ -11,10 +11,47 @@ namespace Menus2
         static void Main(string[] args)
         {
             //variable declaration
-
+            string userInput;
+            Boolean doInput = true;
 
             Console.WriteLine("Please select option from list:"
-                + "\nOption: 1\nOption: 2\nOption: 3\nExit");
+                + "\nOption: 1\nOption: 2\nOption: 3\nExit: e");
+
+            //do userInput = Console.ReadLine();
+            //while (!"1234".Contains(userInput));
+
+            while (doInput) { 
+
+                userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+
+                    case "1":
+                        GuesNumberGame();
+                        break;
+
+                    case "2":
+                        break;
+
+                    case "3":
+                        break;
+
+                    case "e":
+                        doInput = false;
+                        break;
+                }
+            } 
+        }
+
+        static void GuesNumberGame()
+        {
+            Console.WriteLine("\nGuess a Numer Game!");
+
+            Random randGenerator = new Random();         // object randGenerator can generate radom numbers
+            int randNumb = randGenerator.Next(1, 11);
+            //Console.WriteLine(randNumb);
+            Console.WriteLine(randGenerator.Next(1, 11));
 
         }
     }
