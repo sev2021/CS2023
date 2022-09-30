bd730981c6dcbed9aebf8fdb681d66dba0bdea88
sapinskise <19euro@gmail.com>
2022-09-30 09:29:10 +0100

    initial Friday

---
+++ W02D5/Menus2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menus2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable declaration
+
+
+            Console.WriteLine("Please select option from list:"
+                + "\nOption: 1\nOption: 2\nOption: 3\nExit");
+
+        }
+    }
+}
