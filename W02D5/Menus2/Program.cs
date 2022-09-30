5177b2b3d62a87effc18599af2b03c72382c4c39
sapinskise <19euro@gmail.com>
2022-09-30 11:32:02 +0100

    Final

--- W02D5/Menus2/Program.cs
+++ W02D5/Menus2/Program.cs
@@ -46,13 +46,32 @@ namespace Menus2
 
         static void GuesNumberGame()
         {
-            Console.WriteLine("\nGuess a Numer Game!");
+            int playerNumb;
+            int attempts = 5;
 
-            Random randGenerator = new Random();         // object randGenerator can generate radom numbers
+            // object randGenerator can generate radom numbers
+            Random randGenerator = new Random();
             int randNumb = randGenerator.Next(1, 11);
-            //Console.WriteLine(randNumb);
-            Console.WriteLine(randGenerator.Next(1, 11));
+            Console.WriteLine(randNumb);
 
+            while (attempts > 0) { /////////////////////////////// game loop
+                Console.Write($"\nGues a number between 1 and 10.\nYou have {attempts} tries left: ");
+
+
+                while (!int.TryParse(Console.ReadLine(), out playerNumb)) ///// input from player
+                    Console.WriteLine("\nWrong input!\n\nGues a number between 1 and 10: " + playerNumb);
+
+                if (randNumb == playerNumb)  // compare numbers
+                {
+                    Console.WriteLine("\n*********** You Win!! ***********");
+                    break;
+                }
+               
+                if(attempts-- == 0)
+                    Console.WriteLine($"\n *** You lose! Secret number is {randNumb} ***");
+
+            }
+            
         }
     }
 }
