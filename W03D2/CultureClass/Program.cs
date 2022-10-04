bd40eb6c2075b4d566dd0c53dd8ffcaeef42221d
sapinskise <19euro@gmail.com>
2022-10-04 14:56:33 +0100

    initial

---
+++ W03D2/CultureClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CultureClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new Car();
+            Bus b1 = new Bus();
+
+        }
+    }
+
+   
+}
