0155dda4b7360c62603283fee07afc25817dc691
sapinskise <19euro@gmail.com>
2022-10-17 10:14:04 +0100

    first form done

---
+++ W05D1/SalesDBApp/Navigation.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SalesDBApp
+{
+    partial class Navigation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnGoToAdd = new System.Windows.Forms.Button();
+            this.btnGoToFillOrCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "What do you want to do?";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(278, 234);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnGoToAdd
+            // 
+            this.btnGoToAdd.AutoSize = true;
+            this.btnGoToAdd.Location = new System.Drawing.Point(128, 102);
+            this.btnGoToAdd.Name = "btnGoToAdd";
+            this.btnGoToAdd.Padding = new System.Windows.Forms.Padding(15, 0, 15, 0);
+            this.btnGoToAdd.Size = new System.Drawing.Size(123, 28);
+            this.btnGoToAdd.TabIndex = 1;
+            this.btnGoToAdd.Text = "Add an account";
+            this.btnGoToAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnGoToFillOrCancel
+            // 
+            this.btnGoToFillOrCancel.AutoSize = true;
+            this.btnGoToFillOrCancel.Location = new System.Drawing.Point(117, 147);
+            this.btnGoToFillOrCancel.Name = "btnGoToFillOrCancel";
+            this.btnGoToFillOrCancel.Padding = new System.Windows.Forms.Padding(15, 0, 15, 0);
+            this.btnGoToFillOrCancel.Size = new System.Drawing.Size(152, 28);
+            this.btnGoToFillOrCancel.TabIndex = 1;
+            this.btnGoToFillOrCancel.Text = "Fill or cancel an order";
+            this.btnGoToFillOrCancel.UseVisualStyleBackColor = true;
+            // 
+            // Navigation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 279);
+            this.Controls.Add(this.btnGoToFillOrCancel);
+            this.Controls.Add(this.btnGoToAdd);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Navigation";
+            this.Text = "Welcome";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnGoToAdd;
+        private System.Windows.Forms.Button btnGoToFillOrCancel;
+    }
+}
+
--- W05D1/SalesDBApp/Navigation.cs
+++ W05D1/SalesDBApp/Navigation.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SalesDBApp
 {
-    public partial class Form1 : Form
+    public partial class Navigation : Form
     {
-        public Form1()
+        public Navigation()
         {
             InitializeComponent();
         }
